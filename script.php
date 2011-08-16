a52e9fa8af5ea6123381a49bb0269fe78021e4e6
Brian Shaughnessy <brian@lcdservices.biz>
2011-08-15 21:19:24 -0400

    minor cleanup and new readme file

--- script.php
+++ script.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @version     2011-07-23 20:07:15$
- * @author       
+ * @author      Brian Shaughnessy
  * @package     CiviCRM Group Sync
  * @copyright   Copyright (C) 2011- . All rights reserved.
- * @license     
+ * @license     GNU GPL
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
@@ -13,7 +13,13 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
  * Installer script for package
  */
 class com_civiGroupSyncInstallerScript {
-    
+        
+    /**
+     * method to run during installation
+     * installs and enables the plugin
+     *
+     * @return void
+     */
     function install($parent)
     {
         $manifest = $parent->get("manifest");
@@ -42,22 +48,6 @@ class com_civiGroupSyncInstallerScript {
                         AND $columnType = 'plugin'"
                       );
         $db->query();
-        
-    }
-    
-    /**
-     * method to run after an install/update/uninstall method
-     *
-     * @return void
-     */
-    function postflight($type, $parent) 
-    {
-        // enable system plugin after package install
-        /*if ( in_array($type, array('install', 'update', 'discover_install')) ) {
-            $db = JFactory::getDbo();
-            $db->setQuery("UPDATE #__extensions SET enabled = 1 WHERE element = 'civigroupsync' AND type = 'plugin'");
-            $plgEnable = $db->loadResult();
-        }*/
     }
 }
 ?>
